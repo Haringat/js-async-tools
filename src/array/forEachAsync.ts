66d59b8d905431556b95fb6713fc955e4e11f35c
Marcel Mundl <marcel.mundl@lustitia.de>
2017-11-28 18:06:38 +0100

    initial commit

---
+++ src/array/forEachAsync.ts
@@ -0,0 +1,16 @@
+declare global {
+    // noinspection TsLint (built-in type)
+    interface Array<T> {
+        forEachAsync<S>(iterator: (this: S, value: T, index: number, array: this) => Promise<void>, thisArg?: S): Promise<void>;
+    }
+}
+
+Array.prototype.forEachAsync = function<S, T>(
+    iterator: (this: S, value: T, index: number, array: Array<T>) => Promise<void>,
+    thisArg?: S
+) {
+    return Promise.all(this.map(iterator, thisArg))
+        .then(() => undefined);
+};
+
+export {};
---
+++ src/array/mapAsync.ts
@@ -0,0 +1,15 @@
+declare global {
+    // noinspection TsLint (built-in type)
+    interface Array<T> {
+        mapAsync<S, U>(iterator: (this: S, value: T, index: number, array: this) => Promise<U>, thisArg?: S): Promise<Array<U>>;
+    }
+}
+
+Array.prototype.mapAsync = function<S, T, U>(
+    iterator: (this: S, value: T, index: number, array: Array<T>) => Promise<U>,
+    thisArg?: S
+) {
+    return Promise.all<U>(this.map(iterator, thisArg));
+};
+
+export {};
